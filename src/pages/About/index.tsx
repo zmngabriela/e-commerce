5e3e97dba9dd94f1c24e45716c1844c7fe7c887e
zmngabriela <zmngabriela@gmail.com>
2025-02-13 16:22:45 +0100

    reflog: updating about component text content with the new project's features, auth, form management, product image and order creation mocks

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -9,9 +9,14 @@ import { Container } from '../../styles'
 const About = () => (
   <Container className='central narrow'>
       <S.Text>
-        Gabriela is a Java Full Stack Developer student.<br />
+        Gabriela is a Java Full Stack Developer student.
+        <br />
+        <br />
         After some time of self studying, decided to join forces with the school <a href="https://ebaconline.com.br/" target='_blank'>EBAC</a>, to consolidate the knowledge.
+        <br />
+        <br />
         Her background in graphic and fashion design drives her appreciation of beautiful layouts and clean coding, while her hard work and fast leaning allows her to enjoy the process of learning to program.
+        <br />
         A perfect blend for creating quality projects with a a styled design touch.
       </S.Text>
       <S.Icons>
@@ -21,13 +26,20 @@ const About = () => (
         <img src={styledComponents} alt="Styled Components" />
       </S.Icons>
       <S.Text>
-        The technologies used in this project include React with Redux for state management, TypeScript for static typing, styled-components for dynamic styling, Jest/Testing library for testing.<br />
-        The e-commerce website is powered by the Rest <a href="https://fakeapi.platzi.com/" target='_blank'>Platzi Fake Store API</a>, for fetching product data.
-        The API is colaborative and users can post products and categories, hence the visual can apper messy sometimes as there are products with a fallback image, or missing categories. I chose to create this project with a real API and for study purpose, it fits perfectly.
-        Since this API doesn't support features like order creation or product size, a mock solution was implemented to handle them.
+        The technologies used in this project include React with Redux for state management, TypeScript for static typing, and styled-components for dynamic styling. Jest and Testing Library are used for testing, although testing is still a work in progress.
+        <br />
+        <br />
+        The e-commerce website is powered by the Rest <a href="https://fakeapi.platzi.com/" target='_blank'>Platzi Fake Store API</a>, which is used for fetching product and user data, implementing JWT authentication, and handling pagination.
+        <br />
+        <br />
+        To enhance the functionality beyond what the API offers, certain features were custom-built, such as a newsletter subscription and order creation.
+        <br />
+        <br />
+        Additionally, product images were mocked, as the API is collaborative, allowing users to post products and categories, which can result in a visually inconsistent experience.
+        <br />
+        Form management and validation are handled using Formik and Yup.
       </S.Text>
   </Container>
-
 )
 
 export default About
