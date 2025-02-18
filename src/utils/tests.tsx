8a070d14cfb8a31e11ada0cb57b96094d1da195a
zmngabriela <zmngabriela@gmail.com>
2025-02-18 18:08:10 +0100

    feat: adding MemoryRouter to jest render with provider wrapper
    because will be necessary for the unit tests that requires params from route to fetch data

--- src/utils/tests.tsx
+++ src/utils/tests.tsx
@@ -1,13 +1,15 @@
 import React, { PropsWithChildren } from "react"
 import { Provider } from "react-redux";
 import { render, RenderOptions } from "@testing-library/react"
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, MemoryRouterProps } from "react-router-dom";
 
 import { AppStore, configStore, RootState } from "../store";
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-  preloadedState?: Partial<RootState>,
+  preloadedState?: Partial<RootState>
   store?: AppStore
+  useMemoryRouter?: boolean
+  memoryRouterProps?: MemoryRouterProps
 }
 
 export function renderWithProvider(
@@ -15,15 +17,18 @@ export function renderWithProvider(
   {
     preloadedState = {},
     store = configStore(preloadedState),
+    useMemoryRouter = false,
+    memoryRouterProps = {},
     ...aditionalOptions
   }: ExtendedRenderOptions = {}
 ) {
   function Wrapper({children}: PropsWithChildren): JSX.Element {
+    const Router = useMemoryRouter ? MemoryRouter : BrowserRouter
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <Router {...memoryRouterProps}>
           {children}
-        </BrowserRouter>
+        </Router>
       </Provider>
     )
   }
